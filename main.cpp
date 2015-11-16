d09f225182991dac13680bdcb23f42be51c3edf5
Lucien Guimier <lucien.guimier@laposte.net>
2015-11-16 18:42:33 +0100

    Code documentation

--- main.cpp
+++ main.cpp
@@ -101,6 +101,13 @@ vector<bitset<HAMMING_7> > HammingEncoding(vector<bitset<N> > bitsetVector)
 	return encodedBitset;
 }
 
+/*----- Edited code -----*/
+
+/** Get the “G” matrix for Hamming(7,4).
+ * Returns a reference to the matrix, generated only once.
+ * First index is the line number, second index is the column, so that
+ * `line.to_string()` displays in the right order.
+ */
 const vector<bitset<HAMMING_7>>& Hamming7_4Generator() {
 	static vector<bitset<HAMMING_7>> generator;
 	static bool initialized = false;
@@ -115,7 +122,19 @@ const vector<bitset<HAMMING_7>>& Hamming7_4Generator() {
 	return generator;
 }
 
-
+/** Get the *encoded* symbol for a given *non-encoded* symbol.
+ * @param DEC_W
+ *     Length of the *non-encoded* symbol.
+ * @param ENC_W
+ *     Length of the *encoded* symbol.
+ * @param inBuffer
+ *     *Non-encoded* symbol.
+ * @param generator
+ *     Generator matrix. First index is the line number, second index is the
+ *     column, so that `line.to_string()` displays in the right order.
+ * @returns
+ *     *Encoded* symbol.
+ */
 template <size_t DEC_W, size_t ENC_W>
 bitset<ENC_W> GeneratorHammingEncodeSymbol( const bitset<DEC_W>& inBuffer, const vector<bitset<ENC_W>>& generator ) {
 	bitset<ENC_W> outBuffer = 0;
@@ -132,9 +151,20 @@ bitset<ENC_W> GeneratorHammingEncodeSymbol( const bitset<DEC_W>& inBuffer, const
 	return outBuffer;
 }
 
-/**
- * 
- **/
+/** Transform a *non-encoded* stream into an *encoded* stream using a generator
+ * matrix.
+ * @param DEC_W
+ *     Length of the *non-encoded* symbols.
+ * @param ENC_W
+ *     Length of the *encoded* symbols.
+ * @param bitsetVector
+ *     List of *non-encoded* symbols.
+ * @param generator
+ *     Generator matrix. First index is the line number, second index is the
+ *     column, so that `line.to_string()` displays in the right order.
+ * @returns
+ *     List of *encoded* symbols.
+ */
 template <size_t DEC_W, size_t ENC_W>
 vector<bitset<ENC_W> > GeneratorHammingEncoding( vector<bitset<DEC_W> > bitsetVector, const vector<bitset<ENC_W>>& generator )
 {
@@ -154,17 +184,26 @@ vector<bitset<ENC_W> > GeneratorHammingEncoding( vector<bitset<DEC_W> > bitsetVe
 	return encodedBitset;
 }
 
+/** Transform an *encoded* stream into an *non-encoded* stream for Hamming(7,4).
+ * @param bitsetVector
+ *     List of *encoded* symbols.
+ * @returns
+ *     List of *decoded* symbols.
+ */
 vector<bitset<N>> GeneratorHammingDecoding7_4( vector<bitset<HAMMING_7> > bitsetVector )
 {
 	vector<bitset<N> > encodedBitset;
 	
 	if(DEBUG_HE)
 		std::cout << "Decode  :";
-		
+	
+	// Decode each symbol individually.
 	for(vector<bitset<HAMMING_7> >::const_iterator it = bitsetVector.begin(); it != bitsetVector.end(); ++it)
 	{
 		bitset<HAMMING_7> inBuffer = *it;
 		
+		// Compute the syndrom using the fact that the “V” matrix is in
+		// integer order.
 		bitset<HAMMING_7-N> syndrom = 0;
 		for ( int i = 0; i < HAMMING_7; ++i ) {
 			if ( inBuffer[i] != 0 ) {
@@ -176,13 +215,17 @@ vector<bitset<N>> GeneratorHammingDecoding7_4( vector<bitset<HAMMING_7> > bitset
 		
 		if(DEBUG_HE)
 			cout << " | " << intSyndrom;
-			
+		
+		// If the syndrom is not nul, correct the error (hoping there is
+		// only one).
 		if ( intSyndrom != 0 ) {
 			inBuffer[intSyndrom-1] = ! inBuffer[intSyndrom-1];
 		}
 		
 		bitset<N> outBuffer = 0;
 		
+		// Decode the symbol
+		// Non-generic part: use directly the 1-bit-on columns.
 		outBuffer[0] = inBuffer[6];
 		outBuffer[1] = inBuffer[5];
 		outBuffer[2] = inBuffer[4];
@@ -200,22 +243,39 @@ vector<bitset<N>> GeneratorHammingDecoding7_4( vector<bitset<HAMMING_7> > bitset
 	return encodedBitset;
 }
 
+/** Compute the Hamming distance between two symbols.
+ * @param W
+ *     Length of symbols.
+ * @param a
+ *     Reference symbol.
+ * @param b
+ *     Symbol to compare with the reference symbol.
+ * @returns
+ *     Hamming distance between the two symbols.
+ */
 template <size_t W>
 size_t HammingDistance( const bitset<W>& a, const bitset<W>& b ) {
-	size_t res = 0;
-	for ( size_t i = 0; i < W; ++i ) {
-		res += a[i] != b[i];
-	}
-	return res;
+	return ( a ^ b ).count();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                     Main                                                       //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/** Error rate for channel noise simulation. */
 #define ERROR_PROP 0.01
 
-int main_encoder( string fileName ) {
+/** Test encoding and decoding processes.
+ * 
+ * One of the two execution modes: encodes a stream of 4-bits symbols into a
+ * stream of 7-bits symbols using Hamming(7,4), add a random noise and try to
+ * decode it back to 4-bits symbols.
+ * 
+ * @param fileName
+ *     Path of the file to read the 4-bits symbols stream from.
+ * @returns 0
+ */
+int main_encode_decode( string fileName ) {
 	vector< bitset<N> > input_data;
 	vector< bitset<HAMMING_7> > encoded_data;
 	vector< bitset<HAMMING_7> > errored_data;
@@ -224,10 +284,10 @@ int main_encoder( string fileName ) {
 	// Read data to encode
 	input_data = readFile( fileName );
 
-	// Encode by Hamming (7,4) coding
+	// Encode by Hamming(7,4) coding
 	encoded_data = GeneratorHammingEncoding<4,7>( input_data, Hamming7_4Generator() );
 
-	// Inject error
+	// Inject errors
 	srand( time( NULL ) );
 	cout << "Errors  :";
 	for ( vector<bitset<HAMMING_7>>::const_iterator it = encoded_data.begin(); it != encoded_data.end(); ++it ) {
@@ -259,6 +319,13 @@ int main_encoder( string fileName ) {
 	return 0;
 }
 
+/** Compute the minimal Hamming distance of Hamming(7,4).
+ * @param generator
+ *     Generator matrix. First index is the line number, second index is the
+ *     column, so that `line.to_string()` displays in the right order.
+ * @returns
+ *     The minimal Hamming distance between any two *valid* 7-bits symbols;
+ */
 int computeDistance( const vector<bitset<HAMMING_7>>& generator ) {
 	vector< bitset<HAMMING_7> > codes;
 	size_t len = 1 << N;
@@ -281,16 +348,37 @@ int computeDistance( const vector<bitset<HAMMING_7>>& generator ) {
 	return minDist;
 }
 
+/** Display the minimal Hamming distance for Hamming(7,4).
+ * 
+ * One of the two execution modes: computes the minimal distance and print it
+ * the standard output.
+ *
+ * @returns 0
+ */
 int main_distance() {
 	cout << "minimal distance: " << computeDistance( Hamming7_4Generator() ) << endl;
 	
 	return 0;
 }
 
+/** Run the program.
+ * 
+ * Accepted signatures:
+ *   - `<prgm>` displays the minimal Hamming distance of Hamming(7,4).
+ *   - `<prgm> <fileName> encodes, adds noise to and decode the content of the
+ *     given file, then displays statistics about reliability.
+ * 
+ * @param argc
+ *     Program arguments count.
+ * @param argv
+ *     Program arguments.
+ * @return
+ *     0 on success, 1 on error.
+ */
 int main( int argc, char* argv[] ) {
 	switch ( argc ) {
 		case 1: return main_distance();
-		case 2: return main_encoder( argv[1] );
+		case 2: return main_encode_decode( argv[1] );
 		default:
 			cerr << "Unexpected arguments. Valid calls:" << endl
 				<< " ⋅ " << argv[0] << "             Compute minimal Hamming distance" << endl
